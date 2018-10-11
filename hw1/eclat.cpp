e283adeb3c12052df117a3a469fd7afd3814f5ca
Seasa_Huabg <ericet1234@gmail.com>
2018-10-12 00:13:05 +0800

    first commit

---
+++ hw1/eclat.cpp
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<string.h>
+#include<vector>
+
+using namespace std;
+
+class eclat{
+    private:
+        char* input_file;
+        double min_sup;
+    public:
+        eclat(){
+            
+        }
+        eclat(char *input_file,double min_sup){
+            this->input_file = input_file;
+            this->min_sup = min_sup;
+        }
+}
+
+int main(int argc,char * argv[])
+{
+    uint128_t a
+
+
+    
+}
