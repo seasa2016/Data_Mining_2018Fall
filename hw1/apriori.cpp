32699dc52c5d1d3d098866a70e0aaf0f5dff411f
Seasa_Huang <ericet1234@gmail.com>
2018-10-19 11:02:27 +0800

    might finish pass simple test

--- hw1/apriori.cpp
+++ hw1/apriori.cpp
@@ -2,84 +2,200 @@
 #include<string.h>
 #include<vector>
 #include<iostream>
+#include<bitset>
+#include<utility>
+#include<algorithm>
+#include<set>
+#define bit_size 32
 
 using namespace std;
-
-
-class ECLAT{
+class APRIORI{
     private:
-        vector<unsigned __int128> input_data;
+        vector< bitset<bit_size>  > input_data;
         double min_sup;
         char * output_file;
+        vector< pair<vector<int>,int> > ans;
     public:
-        ECLAT(void){
+        APRIORI(void){
             
         }
-        ECLAT(vector<unsigned __int128> &input_data,double min_sup,char* output_file){
-            this->input_data = input_data;
+        APRIORI(vector< bitset<bit_size>  > &input_data,double min_sup,char* output_file){
             this->min_sup = min_sup;
             this->output_file = output_file;
-        }
-
-        int count(unsigned __int128 x)
-        {
-            int num = 0;
-            while(x)
-                if(x%2)
-                {
-                    num ++;
-                    x /= 2;
-                }
-            return num;
-        }
-
-        void freq()
-        {
+            this->ans.clear();
             
+            this->input_data = input_data;
+        }
 
+        vector< vector<int> > count(vector< vector<int> > &check_set)
+        {
+            int num = 0,total=0;
+            int size_check = check_set[0].size();
+            vector< vector<int> > next;
+
+            for(int i=0;i<check_set.size();i++)
+            {
+                total = 0;
+                for(int k=0 ; k<input_data.size() ; k++)
+                {
+                    num = 0;
+                    for(int j=0;j<check_set[i].size();j++)
+                        num += input_data[k][check_set[i][j]];
+                    if(num == size_check)
+                        total ++;
+                }
+                //thus satisfied the constrain
+                if(total>=min_sup)
+                {
+                    next.push_back(check_set[i]);
+                    ans.push_back(pair<vector<int>,int>(check_set[i],total));
+                }
+            }
+
+            return next;
+        }
+        void find(vector< vector<int> > &candidate,vector< vector<int> >&next,vector<int> temp,int idx,int now=0,int max=0)
+        {
+            if(now==0)
+            {
+                //printf("-\n");
+                int arr[bit_size] = {0};
+                for(int i=0;i<temp.size();i++)
+                {
+                    for(int j=0 ; j<candidate[ temp[i] ].size() ; j++)
+                    {
+                        //printf("%d ",candidate[ temp[i] ][j]);
+                        arr[ candidate[ temp[i] ][j] ] ++;
+                    }
+                    //printf("\n");
+                }
+                int num=0;
+                vector<int> dummy(max,0);
+                for(int i=0;i<bit_size;i++)
+                {
+                    //printf("%d ",arr[i]);
+                    if(arr[i] == max-1)
+                    {
+                        dummy[num] = i;
+                        num++;
+                    }   
+                }
+                //printf("\n");
+                //printf("num %d\n",num);
+                if(num == max)
+                    next.push_back(dummy);
+                
+                return ;    
+            }
+            
+            int temp_size = temp.size();
+            temp.push_back(-1);
+
+            for(int i=idx;i<candidate.size();i++)
+            {
+                temp[temp_size] = i;
+                this->find(candidate,next,temp,i+1,now-1,max);
+            }
+        }
+        vector< vector<int> > merge(vector< vector<int> > &candidate,int now=0)
+        {
+            vector< vector<int> > next;
+            vector<int >temp;
+            temp.push_back(-1);
+
+            for(int i=0;i<candidate.size();i++)
+            {
+                temp[0] = i;
+
+                this->find(candidate,next,temp,i+1,now,now+1);
+            }
+
+            return next;
+        }
+        void freq(vector< vector<int> > &candidate,int now=0)
+        {
+            //printf("count\n");
+            candidate = this->count(candidate);
+            //printf("merge\n");
+
+            candidate = this->merge(candidate,now);
+
+            //printf("next\n");
+            if(candidate.size())
+                this->freq(candidate,now+1);
+        }
+        void print()
+        {
+            FILE* output = fopen(this->output_file,"w");
+            //here we finish find the ans 
+            //next we need to do the sort for the ans
+            for(int i=0 ; i<this->ans.size() ; i++)
+            {
+                for(int j=0;j<this->ans[i].first.size();j++)
+                    fprintf(output,"%d ",this->ans[i].first[j]);
+                fprintf(output,"(%d)\n",this->ans[i].second);
+            }
         }
 };
 
 int main(int argc,char * argv[])
 {
-    unsigned __int128 temp;
-    int id;
+    int id,i;
     char str[2048];
     char* fir;
-    vector<unsigned __int128> data;
+    vector< bitset<bit_size> > data;
+    set<int> candidate_temp;
 
     FILE *in;
     //here we first deal with the input data
+    i = 0;
     in = fopen(argv[1],"r");
     
+    bitset<bit_size> temp;
+        
     while(fgets(str,2048,in))
     {
-        temp = 0;
+        temp.reset();
+        //cout<<str;
         fir = strtok(str," ");
         while(fir != NULL)
         {
             sscanf(fir,"%d",&id);
-            temp ^= 1<<(id-1);
+            
+            temp.set(id-1,1);
+            
+            candidate_temp.insert(id-1);
+
             fir = strtok(NULL," ");
         }
         data.push_back(temp);
+        //cout<<temp<<endl;
+        i++;
     }
-    /*
-    printf("%d\n",data.size());
-    for(int i=0;i<data.size();i++)
+    
+    //here take out the candidate
+    
+    //printf("********************\n");
+    vector< vector<int> > candidate;
+    vector<int> member;
+    for(set<int>::iterator it=candidate_temp.begin() ; it!=candidate_temp.end() ; it++)
     {
-        temp = data[i];
-        while(temp)
-        {
-            printf("%d ",temp%2);
-            temp = temp/2;
-        }
-        printf("\n");   
+        member.clear();
+        member.push_back(*it);
+        //printf("%d\n",*it);
+        candidate.push_back(member);
     }
-    */
-   double min_sup;
-   sscanf(argv[2],"%lf",&min_sup);
 
-   ECLAT eclat(data,min_sup,argv[3]);
-   eclat.freq();
+    double min_sup;
+    sscanf(argv[2],"%lf",&min_sup);
+
+    //printf("1********************\n");
+    APRIORI apriori(data,min_sup*i,argv[3]);
+
+    
+    //printf("2********************\n");
+    //cout << head.second;
+    apriori.freq(candidate,1);
+    //printf("3********************\n");
+    apriori.print();
 }
--- hw1/eclat.cpp
+++ hw1/eclat.cpp
@@ -4,48 +4,87 @@
 #include<iostream>
 #include<bitset>
 #include<utility>
+#include<algorithm>
+#define bit_size 512
 using namespace std;
+
 
 
 class ECLAT{
     private:
-        vector< pair< vector<int> , bitset<128> > > input_data;
+        vector< pair< vector<int> , bitset<bit_size> > > input_data;
         double min_sup;
         char * output_file;
-        vector< vector<int> > ans;
+        vector< pair<vector<int>,int> > ans;
     public:
         ECLAT(void){
             
         }
-        ECLAT(vector< pair< vector<int> , bitset<128> > > &input_data,double min_sup,char* output_file){
+        ECLAT(vector< pair< vector<int> , bitset<bit_size> > > &input_data,double min_sup,char* output_file){
             this->min_sup = min_sup;
             this->output_file = output_file;
             this->ans.clear();
             
             this->init(input_data);
         }
-        void init(vector< pair< vector<int> , bitset<128> > > &input_data)
+        void init(vector< pair< vector<int> , bitset<bit_size> > > &input_data)
         {
             //here we first filter out the un sup data
             for(int i=0;i<input_data.size();i++)
                 if(input_data[i].second.count()>this->min_sup)
+                {
                     this->input_data.push_back(input_data[i]);
+                    //printf("min %d\n",input_data[i].first[0]);
+                }
         }
-        void freq(pair< vector<int> , bitset<128> > head,int now)
+        void find(pair< vector<int> , bitset<bit_size> > head,int now)
         {
-            pair< vector<int> , bitset<128> > temp = head;
+            /*
+            printf("\n******************\n");
+            printf("now:%d\nhead: ",now);
+            for(int i=0;i<head.first.size();i++)
+                printf("%d ",head.first[i]);
+            printf("\n");
+            */
+
+            int count;
             for(int i = now;i<this->input_data.size();i++)
             {
+                //printf("%d %d\n",now,i);
+                pair< vector<int> , bitset<bit_size> > temp = head;
                 //this result pass the min sup
                 //add to the final ans
-                if((head.second & this->input_data[i].second).count() >this->min_sup)
+                count = (head.second & this->input_data[i].second).count();
+                if( count > this->min_sup)
                 {
                     temp.first.push_back( this->input_data[i].first[0] );
                     temp.second &= this->input_data[i].second;
-                    this->ans.push_back(temp.first);
+                    this->ans.push_back(pair< vector<int>,int>(temp.first,count));
+                    
+                    /*
+                    printf("next ");
+                    for(int j=0;j<temp.first.size();j++)
+                        printf("%d ",temp.first[j]);
+                    printf("\n");
+                    */
 
-                    this->freq(temp,i+1);
+                    this->find(temp,i+1);
                 }
+            }
+        }
+
+        void freq(pair< vector<int> , bitset<bit_size> > head)
+        {
+            this->find(head,0);
+
+            FILE* output = fopen(this->output_file,"w");
+            //here we finish find the ans 
+            //next we need to do the sort for the ans
+            for(int i=0 ; i<this->ans.size() ; i++)
+            {
+                for(int j=0;j<this->ans[i].first.size();j++)
+                    fprintf(output,"%d ",this->ans[i].first[j]);
+                fprintf(output,"(%d)\n",this->ans[i].second);
             }
         }
 };
@@ -55,7 +94,7 @@ int main(int argc,char * argv[])
     int id,i;
     char str[2048];
     char* fir;
-    vector< pair< vector<int> , bitset<128> > > data;
+    vector< pair< vector<int> , bitset<bit_size> > > data;
 
     FILE *in;
     //here we first deal with the input data
@@ -68,7 +107,7 @@ int main(int argc,char * argv[])
         {
             sscanf(fir,"%d",&id);
             while(data.size()<id)
-                data.push_back( make_pair(vector<int>(1,data.size()+1),bitset<128>()));
+                data.push_back( make_pair(vector<int>(1,data.size()+1),bitset<bit_size>()));
             
             data[id-1].second.set(i);
 
@@ -76,21 +115,26 @@ int main(int argc,char * argv[])
         }
         i++;
     }
-    
+    /*
     cout<<data.size()<<endl;
     
     for(int i=0;i<data.size();i++)
     {
-        cout<< data[i].first[0] << " " << data[i].second <<endl;
+        printf("%3d ",data[i].first[0]);
+        cout<< data[i].second <<endl;
     }
-    
+    */
    
     double min_sup;
     sscanf(argv[2],"%lf",&min_sup);
 
     ECLAT eclat(data,min_sup*i,argv[3]);
 
-    pair< vector<int> , bitset<128> > head; 
-    eclat.freq(head,0);
+    pair< vector<int> , bitset<bit_size> > head; 
+    
+    head.first.clear();
+    head.second.set();
+    //cout << head.second;
+    eclat.freq(head);
    
 }
